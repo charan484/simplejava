ee51d4f4e59ef26bcf8540e12c25a68dfee0d29d
charan484 <sivacharan.anu@gmail.com>
2025-08-20 17:49:40 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, Siva Jenkins Job ready!");
-         System.out.println("Hello, Auto trigger and automate CI builds done!");
+        System.out.println("Hello, Siva Jenkins Job ready release branch!");
+         System.out.println("Hello, Auto trigger and automate CI builds done release branch!");
         System.out.println("Hello, Auto trigger and automate Release branch test build!");
     }
 }
